51a66177009e8dd52272fae7886e6f1d24526b82
RihoSepp31 <riho.sepp@outlook.com>
2024-12-11 02:46:49 +0200

    lisatud 04

---
+++ 04-Function/fun_demo.py
@@ -0,0 +1,11 @@
+def triangle_area_print(a, h):
+    print(a * h / 2)
+
+area = triangle_area_print(10, 10)
+print(area)
+
+def triangle_area(a, h):
+    return a * h / 2
+
+area = triangle_area(10, 10)
+print("area:", area)
